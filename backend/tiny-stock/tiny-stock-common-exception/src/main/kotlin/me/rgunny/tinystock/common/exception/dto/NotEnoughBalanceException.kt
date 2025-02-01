1d6f2b648b3097094d39180fa4748e16986ff468
RGunny <sadog94@gmail.com>
2025-02-01 14:45:26 +0900

    fix(common-exception): NotEnoughBalanceException 수정

--- backend/tiny-stock/tiny-stock-common-exception/src/main/kotlin/me/rgunny/tinystock/common/exception/dto/NotEnoughBalanceException.kt
+++ backend/tiny-stock/tiny-stock-common-exception/src/main/kotlin/me/rgunny/tinystock/common/exception/dto/NotEnoughBalanceException.kt
@@ -1,4 +1,13 @@
 package me.rgunny.tinystock.common.exception.dto
 
-class NotEnoughBalanceException {
-}
+import me.rgunny.tinystock.common.exception.domain.BaseException
+import me.rgunny.tinystock.common.exception.domain.ErrorCode
+
+class NotEnoughBalanceException(
+    val userId: Long,
+    val currentBalance: Long,
+    val withdraw: Long
+) : BaseException(
+    ErrorCode.INSUFFICIENT_BALANCE,
+    "User($userId): balance=$currentBalance, but tried to withdraw=$withdraw"
+)
